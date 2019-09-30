305979f08e9e07ab59a0486898768449bbaeb6a7
IPG <ipg@IPGnoMacBook-puro.local>
2019-09-30 11:07:29 +0900

    refactor: add comment to main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.config['JSON_AS_ASCII'] = False
 # sample_image_uri = 'https://firebasestorage.googleapis.com/v0/b/ipgpushnotifmasterserver.appspot.com/o/Screenshot_20190917-101012.jpg?alt=media&token=bb3fa819-ebfa-41db-84ed-1faa4083f1e2'
 
 # TODO: 本来は、image_uriが存在しない場合、imageが、autochess_resultの物で無かった場合に個別のエラーメッセージを返してやるのが正しい。
+# エラーが帰っているのに、successというstatusが返っている。404, 200など、status codedを
 # GET param: image_uri: string
 @app.route("/")
 def extract_text_from_autochess_image():

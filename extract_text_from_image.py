69a48879491b74bfbf592b641ce3e261e1a8bf7c
t-kabaya <ipg@IPGnoMacBook-puro.local>
2019-10-08 11:25:10 +0900

    refactor: delete unneed comment

--- extract_text_from_image.py
+++ extract_text_from_image.py
@@ -1,23 +1,11 @@
 from google.cloud import vision
 
-# cloud vision apiで、imageからtextを抽出する
-def detect_text_uri(uri):
+def detect_text_uri(image_uri):
     client = vision.ImageAnnotatorClient()
     image = vision.types.Image()
-    image.source.image_uri = uri
+    image.source.image_uri = image_uri
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
 
-    # DEBUG: commentout to debug
-    # print('Texts:')
-    # for text in texts:
-    #     print(text.description)
-    #     print('\n"{}"'.format(text.description))
-
-    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
-    #                 for vertex in text.bounding_poly.vertices])
-
-    #     print('bounds: {}'.format(','.join(vertices)))
-    
     return texts
